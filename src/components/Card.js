9b78aee6f9035734098e6579e238c8fa5ffb7e6c
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-25 16:11:28 +0900

    fixed spacing

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,13 +10,12 @@ function Card(props) {
         <div className="cardDetails">
           <span className='cardLocationDetails'>
           <i class="fa-solid fa-location-dot"></i>
-          <p>{props.item.location}</p>
-          <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer">View on Google Maps</a>
+          <p>J A P A N</p>
+          <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer" className='detailsLink'>View on Google Maps</a>
           </span>
           <h1>{props.item.location}</h1>
           <p>{props.item.startDate} - {props.item.endDate}</p>
           <p>{props.item.description}</p>
-          <hr />
         </div>
       </div>
     </div>
