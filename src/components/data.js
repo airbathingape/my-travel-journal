a2e3fd455ad564741fcacbaa3d38644b2e544acd
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-25 16:38:36 +0900

    added norway to data.js

--- src/components/data.js
+++ src/components/data.js
@@ -17,18 +17,17 @@ const data = [
       endDate: "8 Jun, 2021",
       description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
       imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+  },
+
+    {
+      title: "Geirangerfjord",
+      location: "Norway",
+      googleMapsUrl: "https://goo.gl/maps/EwLb77z3BdoMUvsd9",
+      startDate: "01 Oct, 2021",
+      endDate: "18 Nov, 2021",
+      description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+      imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
   }
 ]
 
 export default data;
-
-// Photos from Unsplash
-// (can use these URLs as your img src)
-
-// Japan
-// https://source.unsplash.com/WLxQvbMyfas
-
-// Australia
-
-// Norway
-// https://source.unsplash.com/3PeSjpLVtLg
