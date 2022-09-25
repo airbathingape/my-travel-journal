1408f85688a978d6ebdd789e4c58eeb1033d80b6
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-25 15:48:02 +0900

    added details from data.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,14 @@ function Card(props) {
           <img src={props.item.imageUrl} alt="map" />
         </div>
         <div className="cardDetails">
-          <span>Map</span>
-          <h1>Location</h1>
-          <p>dates</p>
-          <p>description</p>
+          <span>
+          <i class="fa-solid fa-location-dot"></i>
+          <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer">View on Google Maps</a>
+          </span>
+          <h1>{props.item.location}</h1>
+          <p>{props.item.startDate} - {props.item.endDate}</p>
+          <p>{props.item.description}</p>
+          <hr />
         </div>
       </div>
     </div>
