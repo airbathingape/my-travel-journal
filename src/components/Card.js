003439a31d9394bed48e1010013c6831a4e308b5
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-25 16:04:14 +0900

    fixed image sizing plus centering

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,14 @@ import './Card.css'
 function Card(props) {
   return (
     <div className="cardMain">
-      <div className='card'>
-        <div className="cardImage">
-          <img src={props.item.imageUrl} alt="map" />
+      <div className="card">
+        <div>
+          <img src={props.item.imageUrl} alt="map"  className="cardImage" />
         </div>
         <div className="cardDetails">
-          <span>
+          <span className='cardLocationDetails'>
           <i class="fa-solid fa-location-dot"></i>
+          <p>{props.item.location}</p>
           <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer">View on Google Maps</a>
           </span>
           <h1>{props.item.location}</h1>
